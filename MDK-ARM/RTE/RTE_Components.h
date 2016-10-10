1e7e578378f221b352cb7b00ff2425727e865caf
ivan <zzzof@ya.ru>
2016-10-10 17:33:47 +0400

    added project file

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '02_Emul' 
+ * Target:  '02_Emul' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
